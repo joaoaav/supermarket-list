47e971656b238fdcf03aec273e3bb3d99a8a086c
João Vieira <joao.aav@gmail.com>
2020-02-04 23:49:26 +0100

    WIP Add redux-state-sync

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import rootReducer from "./reducers";
+import { createStore, applyMiddleware } from "redux";
+import {
+  createStateSyncMiddleware,
+  initStateWithPrevTab
+} from "redux-state-sync";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const config = {};
+const middlewares = [createStateSyncMiddleware(config)];
+
+const store = createStore(rootReducer, {}, applyMiddleware(...middlewares));
+initStateWithPrevTab(store);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
